8a56902af348e368b91edce4adc3b37713f39e6b
Константин Прийма <gospoddddd@MacBook-Air-Konstantin.local>
2025-11-18 18:00:24 -0800

    add README and sample Python program

---
+++ sw/hello_git.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Простейшая проверочная программа для шага 2 ЛР (любой код, который легко проверить)."""
+
+def main():
+    name = "Константин"
+    group = "М30-409Б-22"
+    print(f"Hello from Git repo! Автор: {name}, группа: {group}")
+
+if __name__ == "__main__":
+    main()
